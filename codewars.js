501d46516656509281243cc5c33370cd2a8f3d82
Zach Gentner <zach.gentner@gmail.com>
2023-08-24 19:14:09 -0400

    first commit

---
+++ codewars.js
@@ -0,0 +1,17 @@
+//MULTIPLES OF 3 OR 5
+// function solution(number){
+//     //Create a placeholder variable to store the sum of multiples
+//     let result = 0;
+  
+//     //Iterate through all numbers from 1 through number exclusive.
+//     for (let i = 1; i < number; i++) {
+//         //If the current value i is divisible by 3 or 5, add it to the result.
+//         if(i % 3 === 0 || i % 5 === 0){
+//             result += i;
+//         }
+//     }
+    
+//     //Return the sum of all n that are multiples of 3 or 5.
+//     return result;
+//   }
+
