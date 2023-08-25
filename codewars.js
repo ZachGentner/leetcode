bb160708588788b50b8dac58e285d79f9504865b
Zach Gentner <zach.gentner@gmail.com>
2023-08-24 20:13:19 -0400

    update

--- codewars.js
+++ codewars.js
@@ -13,10 +13,31 @@
 //     return result; //Return the sum of all n that are multiples of 3 or 5.
 //   }
 
-//VOWEL COUNT
+// =====================================================================================================================================================================================
+
+//VOWEL COUNT (Regex and String Methods)
 // function getCount(str) {
 //     let arr = str.toLowerCase() //First, we convert the string to lowercase.
 //                  .match(/[aeiou]/g); //Next, we match all instances of "aeiou" throughout the string using the g flag.
 
 //     return arr != null ? arr.length : 0; //Finally, we return the length of the array of characters if arr is not null.
 //   }
+    
+    
+// =====================================================================================================================================================================================
+
+//EVEN OR ODD (Ternary Solution)
+// function evenOrOdd(number) {
+    //   return number % 2 === 0 ? "Even" : "Odd"; //Here, we use the conditional (ternary) operator to return either Even or Odd if n % 2 equals 0.
+    // }
+    
+// EVEN OR ODD (If-Else Solution)
+// function evenOrOdd(number) {
+//     if (number % 2 === 0) {
+//         return "Even";
+//     } else {
+//         return "Odd";
+//     }
+// }
+
+// =====================================================================================================================================================================================
